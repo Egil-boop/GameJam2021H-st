6121919d279633d3fc695dfd2a98e2657cd46451
yaouOps <72212799+yaouOps@users.noreply.github.com>
2021-10-19 19:02:40 +0200

    playerstate in progress

---
+++ GameJam2021H/Assets/Scripts/UI/DamagePopUp.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DamagePopUp : MonoBehaviour
+{
+
+	public PlayerState state;
+	public Text[] damagePopUps;
+
+	public float speed = 0.1f;
+	public float duration = 1f;
+	public Vector3 startPosition;
+
+    private void FixedUpdate()
+    {
+        foreach(Text text in damagePopUps)
+        {
+            if (text.enabled)
+            {
+				Color disableColor = new Color(text.color.r, text.color.g, text.color.r, 0);
+
+				text.gameObject.transform.position += new Vector3(0, speed);
+				text.color = Color.Lerp(text.color, disableColor, duration);
+            }
+
+        }
+    }
+
+    public void Pop(int damage)
+	{
+		Text textToPop = null;
+
+		for (int i = 0; i < damagePopUps.Length; i++)
+		{
+			if (!damagePopUps[i].enabled)
+			{
+				textToPop = damagePopUps[i];
+			}
+		}
+
+		if(textToPop == null)
+        {
+			textToPop = damagePopUps[0];
+			Debug.Log("Damage Pop Ups are being recycled");
+        }
+
+		textToPop.enabled = true;
+		textToPop.gameObject.transform.position = startPosition;
+		textToPop.color = state.playerColor;
+		textToPop.text = "-$" + damage;
+	}
+}
