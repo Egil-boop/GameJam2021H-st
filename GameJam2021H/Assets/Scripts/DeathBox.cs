2e5eaece1dc0967e226302f6ec7d687217acfec4
yaouOps <72212799+yaouOps@users.noreply.github.com>
2021-10-20 12:54:13 +0200

    player death

---
+++ GameJam2021H/Assets/Scripts/DeathBox.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathBox : MonoBehaviour
+{
+    private BoxCollider2D col;
+
+    void Start()
+    {
+        col = GetComponent<BoxCollider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.TryGetComponent(out PlayerState player))
+        {
+            player.Die();
+        }
+    }
+}
--- GameJam2021H/Assets/Scripts/Player/PlayerState.cs
+++ GameJam2021H/Assets/Scripts/Player/PlayerState.cs
@@ -7,16 +7,27 @@ public class PlayerState : MonoBehaviour
 {
     public DamagePopUp damagePopUp;
     public Text healthUI;
+    public Transform respawnPoint;
 
     public Color playerColor;
     public int startHealth = 1000;
     private int currentHealth;
+    private Rigidbody2D rb;
+
+    public int startLives = 3;
+    private int currentLives;
+
+    public float deathFreezeTimer = 2f;
+    private float dieTimer;
 
     private void Start()
     {
         currentHealth = startHealth;
+        currentLives = startLives;
         healthUI.color = playerColor;
         healthUI.text = "$" + startHealth;
+
+        rb = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
@@ -28,10 +39,46 @@ public class PlayerState : MonoBehaviour
         }
     }
 
+    private void FixedUpdate()
+    {
+        if(dieTimer > 0f)
+        {
+            rb.velocity = Vector2.zero;
+            dieTimer -= Time.deltaTime;
+        }
+    }
+
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
         damagePopUp.Pop(damage);
         healthUI.text = "$" + currentHealth;
+
+        if(currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        Debug.Log("Player died!");
+
+        currentLives--;
+        if(currentLives <= 0)
+        {
+            //game over
+            Debug.LogWarning("TODO: implement GAME OVER!");
+            return;
+        }
+        else
+        {
+            currentHealth = startHealth;
+        }
+
+        transform.position = respawnPoint.position;
+        Debug.LogWarning("TODO: make player invulnerable upon death");
+
+        dieTimer = deathFreezeTimer;
     }
 }
